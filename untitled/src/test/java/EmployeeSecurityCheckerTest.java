26473f913055b1d86c2cc2a0542f9a8e915b80d9
nobojjhb025 <nobojjhb025@student.wethinkcode.co.za>
2026-09-10 20:09:28 +0200

    Feat: Organize project structure and add unit tests

---
+++ untitled/src/test/java/EmployeeSecurityCheckerTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+import za.co.wethinkcode.EmployeeSecurityChecker;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmployeeSecurityCheckerTest {
+
+    @Test
+    void shouldReturnLowRiskForScoreThree() {
+
+        String result = EmployeeSecurityChecker.getRiskLevel(3);
+
+        assertEquals("LOW RISK", result);
+    }
+
+    @Test
+    void shouldReturnMediumRiskForScoreTwo() {
+
+        String result = EmployeeSecurityChecker.getRiskLevel(2);
+
+        assertEquals("MEDIUM RISK", result);
+    }
+
+    @Test
+    void shouldReturnHighRiskForScoreOne() {
+
+        String result = EmployeeSecurityChecker.getRiskLevel(1);
+
+        assertEquals("HIGH RISK", result);
+    }
+
+    @Test
+    void shouldReturnHighRiskForScoreZero() {
+
+        String result = EmployeeSecurityChecker.getRiskLevel(0);
+
+        assertEquals("HIGH RISK", result);
+    }
+}
